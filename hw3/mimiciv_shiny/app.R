01c3e764c4012b11aee718ea07f2d808326b331d
ogolston <oliviagolston@gmail.com>
2021-03-01 00:36:17 +0000

    Created lab events input and output

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -3,9 +3,15 @@ library(tidyverse)
 
 #icu_cohort = readRDS("hw3/mimiciv_shiny/icu_cohort.rds")
 
-# Define UI for miles per gallon app ----
+
+#Ideas: add option for pie chart? 
+#Bivariate Distributions?
+#Missing Data? 
+
+
 ui <- navbarPage("MIMIC-IV Data Dashboard",
   tabPanel("Admissions",
+     titlePanel("Admissions Data"),
      sidebarLayout(
          sidebarPanel(
          selectInput("admit_var", "Choose variable of interest", 
@@ -16,42 +22,68 @@ ui <- navbarPage("MIMIC-IV Data Dashboard",
                      selected = "First Care Unit")
        ),
        mainPanel(
-         plotOutput(outputId = "distPlot")
+         plotOutput(outputId = "admitPlot")
        )
     )   
   ),
+  
   tabPanel("Patients",
     titlePanel("Patients Data"),
     sidebarLayout(
+      sidebarPanel( ),
+      mainPanel(
+        #plotOutput(outputId = "patientPlot")
+      )
+    )
+  ),
+  
+  tabPanel("Lab Events",
+    titlePanel("Lab Event Data"),
+    sidebarLayout(
+      sidebarPanel( 
+        selectInput("lab_var", "Choose lab measurement of interest", 
+                    choices = list("Bicarbonate",
+                                   "Calcium",
+                                   "Chloride",
+                                   "Creatinine",
+                                   "Glucose",
+                                   "Magnesium",
+                                   "Potassium",
+                                   "Sodium",
+                                   "Hemacrit",
+                                   "White Blood Cells",
+                                   "Lactate"),
+                    selected = "Bicarbonate")
+      ),
+      mainPanel(
+        plotOutput(outputId = "labPlot")
+      )
+    )
+  ),
+  
+  tabPanel("Chart Events",
+    titlePanel("Chart Event Data"),
+    sidebarLayout(
       sidebarPanel(
       ),
-    mainPanel()
+
+      mainPanel(
+        #plotOutput(outputId = "chartPlot")
+      )
     )
+  ),
+  
+  tabPanel("Bivariate Distributions",
+    titlePanel("Bivariate Distributions"),
+    sidebarLayout(
+      sidebarPanel(
+      ),
+      
+      mainPanel(
+        #plotOutput(outputId = "bivariatePlot")
+      )
+    )         
   )
-  # tabPanel("Lab Events",
-  #   titlePanel("Lab Event Data"),
-  #   sidebarLayout(
-  #     sidebarPanel(
-  #       #sliderInput()
-  #     ),
-  #    
-  #     mainPanel(
-  #       plotOutput(outputId = "distPlot")
-  #     )
-  #   )        
-  # ),
-  # tabPanel("Chart Events",
-  #   titlePanel("Chart Event Data"),
-  #   sidebarLayout(
-  #     sidebarPanel(
-  #       #sliderInput()
-  #     ),
-  #     
-  #     mainPanel(
-  #       plotOutput(outputId = "distPlot")
-  #     )
-  #   )         
-  #)
 )
   
   
@@ -60,13 +92,7 @@ ui <- navbarPage("MIMIC-IV Data Dashboard",
 
 server <- function(input, output) {
 
-  output$distPlot <- renderPlot({
-    # data <- switch(input$admit_var,
-    #                "First Care Unit" = icu_cohort$first_careunit,
-    #                "Last Care Unit" = icu_cohort$last_careunit,
-    #                "Admission Type" = icu_cohort$admission_type,
-    #                "Admission Location" = icu_cohort$admission_location)
-    
+  output$admitPlot <- renderPlot({
     data <- switch(input$admit_var,
                    "First Care Unit" = "first_careunit",
                    "Last Care Unit" = "last_careunit",
@@ -77,6 +103,24 @@ server <- function(input, output) {
       geom_bar(aes_string(data))
     })
   
+  
+  output$labPlot <- renderPlot({
+    data <- switch(input$lab_var,
+                   "Bicarbonate" = "bicarbonate",
+                   "Calcium" = "calcium",
+                   "Chloride" = "chloride",
+                   "Creatinine" = "creatinine",
+                   "Glucose" = "glucose",
+                   "Magnesium" = "magnesium",
+                   "Potassium" = "potassium",
+                   "Sodium" = "sodium",
+                   "Hemacrit" = "hemacrit",
+                   "White Blood Cells" = "wbc",
+                   "Lactate" = "lactate")
+    
+    ggplot(icu_cohort) +
+      geom_histogram(aes_string(data))
+  })
    
 }
 
