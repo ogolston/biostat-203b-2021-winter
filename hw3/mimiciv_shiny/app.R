4e1af22c4ca6ccb1081085119d66e5897134b1e2
ogolston <oliviagolston@gmail.com>
2021-03-02 16:51:23 +0000

    Continued refactoring code such that variable lists are reusable. Created bivariate plot page.

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -8,6 +8,52 @@ library(tidyverse)
 #Bivariate Distributions?
 #Missing Data? 
 
+#Create list of variable names and meanings for each category, for use in 
+#dropdown menus 
+
+admissions_list <- list("First Care Unit" = "first_careunit",
+                        "Last Care Unit" = "last_careunit",
+                        "Admission Type" = "admission_type",
+                        "Admission Location" = "admission_location")
+
+
+
+patient_list <-  list("Insurance" = "insurance",
+                       "Language" = "langauge",
+                       "Marital Status" = "marital_status",
+                       "Ethnicity" = "ethnicity",
+                       "Gender" ="gender",
+                       "Age" = "age_at_adm",
+                       "Death in 30 Days?" = "death_in_month")
+
+
+lab_list <-  list("Bicarbonate" = "bicarbonate",
+                  "Calcium" = "calcium",
+                  "Chloride" = "chloride",
+                  "Creatinine" = "creatinine",
+                  "Glucose" = "glucose",
+                  "Magnesium" = "magnesium",
+                  "Potassium" = "potassium",
+                  "Sodium" = "sodium",
+                  "Hemacrit" = "hemacrit",
+                  "White Blood Cells" = "wbc",
+                  "Lactate" = "lactate")
+
+
+chart_list <- list("Heart Rate" = "heart_rate",
+                   "Systolic BP (non-invasive)" 
+                   = "non_invasive_blood_pressure_systolic",
+                   "Mean BP (non-invasive)"= 
+                     "non_invasive_blood_pressure_mean",
+                   "Respiratory Rate" = "respiratory_rate",
+                   "Temperature (F)" = "temperature_fahrenheit",
+                   "Systolic BP (arterial)" =
+                     "arterial_blood_pressure_systolic",
+                   "Mean BP (arterial)" =
+                     "arterial_blood_pressure_mean")
+
+
+
 
 ui <- navbarPage("MIMIC-IV Data Dashboard",
   tabPanel("Admissions",
@@ -15,10 +61,7 @@ ui <- navbarPage("MIMIC-IV Data Dashboard",
      sidebarLayout(
          sidebarPanel(
          selectInput("admit_var", "Choose variable of interest", 
-                     choices = list("First Care Unit" = "first_careunit",
-                                    "Last Care Unit" = "last_careunit",
-                                    "Admission Type" = "admission_type",
-                                    "Admission Location" = "admission_location"),
+                     choices = admissions_list,
                      selected = "first_careunit")
        ),
        mainPanel(
@@ -32,13 +75,7 @@ ui <- navbarPage("MIMIC-IV Data Dashboard",
     sidebarLayout(
       sidebarPanel( 
         selectInput("patient_var", "Choose variable of interest",
-                    choices = list("Insurance" = "insurance",
-                                   "Language" = "langauge",
-                                   "Marital Status" = "marital_status",
-                                   "Ethnicity" = "ethnicity",
-                                   "Gender" ="gender",
-                                   "Age" = "age_at_adm",
-                                   "Death in 30 Days?" = "death_in_month"),
+                    choices = patient_list,
                     selected = "insurance")
       ),
       
@@ -53,17 +90,7 @@ ui <- navbarPage("MIMIC-IV Data Dashboard",
     sidebarLayout(
       sidebarPanel( 
         selectInput("lab_var", "Choose lab measurement of interest", 
-                    choices = list("Bicarbonate" = "bicarbonate",
-                                   "Calcium" = "calcium",
-                                   "Chloride" = "chloride",
-                                   "Creatinine" = "creatinine",
-                                   "Glucose" = "glucose",
-                                   "Magnesium" = "magnesium",
-                                   "Potassium" = "potassium",
-                                   "Sodium" = "sodium",
-                                   "Hemacrit" = "hemacrit",
-                                   "White Blood Cells" = "wbc",
-                                   "Lactate" = "lactate"),
+                    choices = lab_list,
                     selected = "bicarbonate")
       ),
       
@@ -78,17 +105,7 @@ ui <- navbarPage("MIMIC-IV Data Dashboard",
     sidebarLayout(
       sidebarPanel(
         selectInput("chart_var", "Choose chart value of interest", 
-                    choices = list("Heart Rate" = "heart_rate",
-                                   "Systolic BP (non-invasive)" 
-                                        = "non_invasive_blood_pressure_systolic",
-                                   "Mean BP (non-invasive)"= 
-                                     "non_invasive_blood_pressure_mean",
-                                   "Respiratory Rate" = "respiratory_rate",
-                                   "Temperature (F)" = "temperature_fahrenheit",
-                                   "Systolic BP (arterial)" =
-                                     "arterial_blood_pressure_systolic",
-                                   "Mean BP (arterial)" =
-                                     "arterial_blood_pressure_mean"),
+                    choices = chart_list,
                     selected = "heart_rate")
       ),
 
@@ -102,10 +119,21 @@ ui <- navbarPage("MIMIC-IV Data Dashboard",
     titlePanel("Bivariate Distributions"),
     sidebarLayout(
       sidebarPanel(
+        selectInput("var1", "Choose variable for x-axis", 
+                    choices = c(lab_list, chart_list),
+                    selected = "heart_rate"),
+        
+        selectInput("var2", "Choose variable for y-axis", 
+                    choices = c(lab_list, chart_list),
+                    selected = "respiratory_rate"),
+        
+        selectInput("var3", "Optional: Choose variable for color", 
+                    choices = c(patient_list, admissions_list),
+                    selected = "gender")
       ),
       
       mainPanel(
-        #plotOutput(outputId = "bivariatePlot")
+        plotOutput(outputId = "bivariatePlot")
       )
     )         
   )
@@ -148,6 +176,18 @@ server <- function(input, output) {
       geom_histogram(aes_string(data))
     
   })
+  
+  
+  output$bivariatePlot <- renderPlot({
+    var1 <- input$var1
+    var2 <- input$var2
+    var3 <- input$var3
+    
+    ggplot(icu_cohort) +
+      geom_point(aes_string(var1, var2, color=var3))
+    
+  })
+  
 }
 
 
